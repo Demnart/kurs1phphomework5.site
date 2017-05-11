119453f81afcf0cb8d864a3cbce6b505f6a1494e
Genjo <insidia93@gmail.com>
2017-05-11 09:37:21 +0300

    Создали обработчик который будет совершать у нас все проверки на наличие зарегистрированного пользователя. function checkLoginAndPassword($login,$password) - данная функция возвращает нам правду если логин и пароль совпадают. function login($login)- Данння функция устанавливает cookies в том случае если логин и пароль совпали с тем, который у нас находится в массиве if (empty($_POST['login'])||empty($_POST['password'])) - проверка на то пришли, ли нам данные с формы if (!checkLoginAndPassword($login,$password)) - проверка на то совпадают ли данные введенные в фоме с данными в массиве логинов и паролей

--- login.php
+++ login.php
@@ -1,6 +1,36 @@
 <?php
+function checkLoginAndPassword($login,$password){
+    $users = ['artem'=>'123','alina'=>'456'];
+    return isset($users[$login]) && $password == $users[$login];
+}
+
+function login($login){
+    setcookie('auth',$login,time()+86400);
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 
 if (empty($_POST['login'])||empty($_POST['password'])){
     header('Location: /form.html');
     exit;
 }
+$login  = $_POST['login'];
+$password = $_POST['password'];
+
+
+if (!checkLoginAndPassword($login,$password)){
+    header('Location: /form.html');
+    exit;
+}
+
+login($login);
+
+
+
